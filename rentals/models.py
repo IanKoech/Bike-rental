4ee4d486a1be24b68711924c961acb787ab2d052
Ian K <iankoech9@gmail.com>
2021-02-02 20:11:53 +0300

    Added default view to return available renterss

--- rentals/models.py
+++ rentals/models.py
@@ -2,3 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Renter(models.Model):
+    first_name = models.CharField(max_length=30)
+    second_name = models.CharField(max_length=30)
+    contact = models.CharField(max_length=10)
+    charges = models.IntegerField()
+    location = models.CharField(max_length=15)
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'images/', null= True)
+    
+
+    def __str__(self):
+        return self.first_name
--- rentals/views.py
+++ rentals/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
-# from .models import Renter
+from .models import Renter
 
 # Create your views here.
 def home(request):
-   return HttpResponse('Ollies bike rental')
+   return render(request, 'home.html')
 
-# def search_results(request):
+def home(request):
+    renters = Renter.objects.all()
+    return render(request, 'home.html', {"renters":renters}) 
 
-#     if 'location' in request.GET and request.GET["location"]:
-#         search_term = request.GET.get("location")
-#         searched_bikes = Renter.search_location(search_term)
-#         message = f"{search_term}"
+def search_results(request):
 
-#         return render(request, 'search.html',{"message":message,"bikes": searched_bikes})
+    if 'location' in request.GET and request.GET["location"]:
+        search_term = request.GET.get("location")
+        searched_bikes = Renter.search_location(search_term)
+        message = f"{search_term}"
 
-#     else:
-#         message = "You haven't entered a location"
-#         return render(request, 'search.html',{"message":message})
+        return render(request, 'search.html',{"message":message,"bikes": searched_bikes})
+
+    else:
+        message = "You haven't entered a location"
+        return render(request, 'search.html',{"message":message})
