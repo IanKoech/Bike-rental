fab56e7436de84d318b3dd9670c65cee9ea90e1a
Ian K <iankoech9@gmail.com>
2021-02-03 14:41:42 +0300

    Created comment form and updated view for comment form

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Renter, Customer
+from .models import Renter, Customer, Comment
 #Register your models here.
 
 admin.site.register(Renter)
 admin.site.register(Customer)
+admin.site.register(Comment)
--- rentals/views.py
+++ rentals/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 from .models import Renter
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -20,3 +22,23 @@ def search_results(request):
     else:
         message = "You haven't entered a location"
         return render(request, 'search.html',{"message":message})
+
+def post_comment(request):
+    renter = get_object_or_404(Renter, slug=slug)
+    comments = renter.comments.filter(active = True)
+    new_comment  = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment form without saving to database
+            new_comment = comment_form.save(commit=False)
+            #Assign current renter to the comment
+            new_comment.renter = renter
+            # saving comment  to database
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, 'comment.html',{'renter':renter, 'comments':comments, 'new_comment':new_comment, 'comment_form':comment_form})
